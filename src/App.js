a9498d09d519b6aa55fff6a08836524318caa5b4
Sean Delaney <hello@delaneymethod.com>
2021-04-09 11:33:46 +0100

    Basic setup
    Signed-off-by: Sean Delaney <hello@delaneymethod.com>

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+import React, { Component, Suspense } from 'react';
+import { BrowserRouter, NavLink, Route } from 'react-router-dom';
 
-import Posts from './containers/Posts';
-import User from './containers/User';
 import Welcome from './containers/Welcome';
 
+const User = React.lazy(() => import('./containers/User').then(component => component));
+const Posts = React.lazy(() => import('./containers/Posts').then(component => component));
+
 class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <React.Fragment>
-          <nav>
-            <NavLink to="/user">User Page</NavLink> |&nbsp;
-            <NavLink to="/posts">Posts Page</NavLink>
-          </nav>
-          <Route path="/" component={Welcome} exact />
-          <Route path="/user" component={User} />
-          <Route path="/posts" component={Posts} />
-        </React.Fragment>
-      </BrowserRouter>
-    );
-  }
+	render() {
+		return (
+			<BrowserRouter>
+				<React.Fragment>
+					<nav>
+						<NavLink to="/user">User Page</NavLink> |&nbsp;
+						<NavLink to="/posts">Posts Page</NavLink>
+					</nav>
+					<Route path="/" component={Welcome} exact/>
+					<Route path="/user" render={() => (
+						<Suspense fallback={<div>Loading...</div>}>
+							<User/>
+						</Suspense>
+					)}/>
+					<Route path="/posts" render={() => (
+						<Suspense fallback={<div>Loading...</div>}>
+							<Posts/>
+						</Suspense>
+					)}/>
+				</React.Fragment>
+			</BrowserRouter>
+		);
+	}
 }
 
 export default App;
